840f52d01e1a81a1573335025229a049a940c73c
mana <nouri393914@gmail.com>
2024-06-15 21:38:09 +0900

    addtional add

--- app/controllers/top_pages_controller.rb
+++ app/controllers/top_pages_controller.rb
@@ -1,3 +1,4 @@
 class TopPagesController < ApplicationController
     def top; end
+    def about; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "top_pages#top"
+  get "/about" => "top_pages#about"
 
   resources :awards, only: %i[index show new create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
